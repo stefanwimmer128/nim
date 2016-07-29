c8f836c8122ccf717677292bcc9bbb2e302fd854
Stefan Wimmer <stefanwimmer128@gmail.com>
2016-07-30 00:13:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# NIM
+
+NIM is an old mathematical game.
+
+# How to play
+
+In each turn you take any number of cards in the same row.
+
+Then press the "AI Turn" button to let the AI play, or let a second player remove cards.
+
+Whoever takes the last card(s) wins.
---
+++ nim.js
@@ -0,0 +1,96 @@
+/*
+ * Copyright (c) 2016, Stefan Wimmer
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/**
+ * Some created JavaScript file
+ * @author Stefan Wimmer <stefanwimmer128@gmail.com>
+ * @date 29.07.16
+ */
+
+$(() =>
+{
+    const rows = [
+        [
+            true, true, true, true, true, true, true
+        ],
+        [
+            true, true, true, true, true
+        ],
+        [
+            true, true, true
+        ],
+        [
+            true
+        ]
+    ];
+    
+    const update = () =>
+    {
+        $("#nim").empty();
+        
+        rows.forEach((row, ir) =>
+        {
+            $("#nim").append("<p></p>");
+            
+            row.forEach((card, ic) =>
+            {
+                if (card)
+                    $("#nim").children("p")
+                        .last()
+                        .append("<span></span>")
+                        .children("span")
+                        .last()
+                        .click(() =>
+                        {
+                            rows[ir][ic] = false;
+                            
+                            update();
+                        });
+            });
+        });
+    };
+    
+    const aiTurn = () =>
+    {
+        const row = Math.floor(Math.random() * rows.length);
+        const cards = rows[row].filter(card => card);
+        
+        if (cards.length < 1)
+            return aiTurn();
+        
+        const takes = Math.floor(Math.random() * cards.length) + 1;
+        
+        let took = 0;
+        
+        rows[row] = rows[row].map(card =>
+        {
+            if (card)
+                if (took <= takes)
+                {
+                    card = false;
+                    took++;
+                }
+                
+            return card;
+        });
+        
+        update();
+    };
+    
+    $("#aiTurn").click(aiTurn);
+    
+    update();
+});
